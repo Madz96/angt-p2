55b59dabb5f562470cbb7023027d6453238b2af4
Madz96 <mad.subasinghe@gmail.com>
2019-08-12 11:22:31 +0530

    Initial commit with lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MainComponent } from "angt-lib";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  title = 'angt-p2';
+  title = "angt-p2";
 }
